46b0f280721c7de05ce28b1b05c5c836c03e27a2
Nicholas Antonio <Nicholas@hexes.dev>
2026-04-17 22:49:28 -0700

    Updated structure

--- src/data/read_sensor_logs.py
+++ src/data/read_sensor_logs.py
@@ -32,49 +32,67 @@ if __name__ == "__main__":
         # store every gps entry time in a hash along with its timestamp
         time_hash = {}
         for gps_entry in gps_entries:
-            time_hash[gps_entry.utc_time] = gps_entry.timestamp
+            time_hash[float(gps_entry.utc_time)] = gps_entry.timestamp
 
-        samples_aligned = False
-        max_count = 0
+        time_modifier = 0
+        prev_gps_second = 0
+        is_first = True
+        share_previous_gap = False
 
-        for index, gps_second in enumerate(time_hash.keys()):
-            samples = [imu for imu in imu_entries if float(imu.gps_second) == float(gps_second)]
-            count = len(samples)
-            max_count = max(count, max_count)
-            print(f"\tScanning timestamps {index+1}/{len(time_hash.keys())} -- Stamp: {gps_second} -- Samples this stamp: {count} -- Max samples per stamp: {max_count}", end="\r", flush=True)
-            if count > 650:
-                anchor_imu = samples[0]
-                print("\n\tAligning IMU samples using source time offset.")
+        for index, imu_entry in enumerate(imu_entries):
 
-                print(f"\t{anchor_imu.source_time}")
-                offset_seconds = time_hash[gps_second].timestamp() - (anchor_imu.source_time / 1e6)
-                for e in imu_entries:
-                    e.timestamp = datetime.fromtimestamp(e.source_time / 1e6 + offset_seconds)
+            if index % 1000 == 0:
+                print(f"\tAligning timestamps {index}/{len(imu_entries)} -- {100 * (index/len(imu_entries)):.2f}%", end='\r', flush=True)
 
-                samples_aligned = True
-                break
+            # skip entries that don't match a gps entry
+            if not float(imu_entry.gps_second) in time_hash:
+                imu_entry.timestamp = None
+                continue
 
-        if not samples_aligned:
-            print(f"\tAligning IMU samples via GPS second matching.")
-            # If no anchor group is found, use the original relative alignment logic
-            for index, imu_entry in enumerate(imu_entries):
-                if imu_entry.gps_second in time_hash:
-                    timestamp = time_hash.get(imu_entry.gps_second)
-                    imu_entry.timestamp = timestamp + timedelta(microseconds=int(imu_entry.gps_time_elapsed))
-                else:
-                    imu_entries[index].timestamp = None
+            # Check if this is the first in the series
+            if not imu_entry.gps_second == prev_gps_second:
+
+                # If previous entries were modified we need to shift them by half the delta between the first in the new second
+                if time_modifier > 0:
+                    timestamp = time_hash.get(imu_entry.gps_second) + timedelta(microseconds=int(imu_entry.gps_time_elapsed))
+                    dt = (timestamp - imu_entries[index - 1].timestamp).total_seconds()
+
+                    for matched_imu_entry in imu_entries:
+                        if matched_imu_entry.gps_second == prev_gps_second:
+                            matched_imu_entry.timestamp += timedelta(seconds=float(dt / 2))
+
+                    time_modifier = 0
+
+                prev_gps_second = imu_entry.gps_second
+
+            imu_entry.timestamp = time_hash.get(imu_entry.gps_second)
+            imu_entry.timestamp += timedelta(microseconds=int(imu_entry.gps_time_elapsed))
+            imu_entry.timestamp += timedelta(seconds=int(time_modifier))
+
+            if index == 0 or imu_entries[index - 1].timestamp is None:
+                continue
+
+            # calculate the time elapsed between samples
+            dt = (imu_entry.timestamp - imu_entries[index - 1].timestamp).total_seconds()
+
+            # if time is negative roll delta into this second
+            if dt < 0:
+                time_modifier = -1 * dt
+                imu_entry.timestamp += timedelta(seconds=int(time_modifier))
+
 
         # some imu readings were taken before valid fix and may not have a matching gps entry
         # filter entries that do not have valid matches
         imu_entries = [entry for entry in imu_entries if entry.timestamp is not None]
 
         total_gps_entries += len(gps_entries)
+        print(f"\033[K", flush=True)
 
         print(f"\t{len(imu_entries)} matched across {len(gps_entries)} GPS entries."
               f"\n\tAverage IMU rate {len(imu_entries) / len(gps_entries)}"
               f"\n\t{len(gps_entries) / 60 :.2f} minutes this entry. {total_gps_entries / 60 :.2f} minutes total."
               f"\n\tStarting Time: {gps_entries[0].timestamp}"
-              f"\n\tEnding Time: {imu_entries[-1].timestamp}")
+              f"\n\tEnding Time: {imu_entries[-1].timestamp}", flush=True)
 
         if args.check_db:
             gps_entry = db.read_gps(time=gps_entries[-1].timestamp)
